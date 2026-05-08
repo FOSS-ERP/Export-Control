28888cfe52135a554ee8aa323fbcf69e75feaba7
basawaraj-fosserp <103717083+basawaraj-fosserp@users.noreply.github.com>
2026-05-08 16:33:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,7 @@ from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
-
-# get version from __version__ variable in export_control/__init__.py
-from export_control import __version__ as version
-
+	
 setup(
 	name="export_control",
 	version=version,
