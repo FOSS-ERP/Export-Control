879e1f0f72a8f753702b2d798fb9878dc8d494d2
basawaraj-fosserp <103717083+basawaraj-fosserp@users.noreply.github.com>
2026-05-08 16:41:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
-	
+    install_requires = f.read().strip().split("\n")
+
+from export_control import __version__ as version
+
 setup(
-	name="export_control",
-	version=version,
-	description="Export Control",
-	author="Bonito Designs",
-	author_email="info@bonito.in",
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
+    name="export_control",
+    version=version,
+    description="Export Control",
+    author="Bonito Designs",
+    author_email="info@bonito.in",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires
 )
